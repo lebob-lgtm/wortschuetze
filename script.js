a7b91fe7220dd20588024c282bb2f4ced8591f9e
lebob-lgtm <julianubeda3@gmail.com>
2025-12-12 13:47:09 +0100

    Update script.js

--- script.js
+++ script.js
@@ -254,7 +254,7 @@
     // Mot terminé → explosion + score + suppression du mot
     if (target.remaining.length === 0) {
       playExplosion();
-      score += 5;
+      score += 999999999;
       enemies = enemies.filter(e => e !== target);
     }
 
@@ -528,6 +528,7 @@
   });
 
 })();
+
 
 
 
